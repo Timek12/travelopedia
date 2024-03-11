e23f194bd51df4f62a6709b1aee6fc9855edbecf
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-11 19:35:14 +0100

    Create destination api using redux toolkit

---
+++ src/api/destinationApi.jsx
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const destinationAPI = createApi({
+  reducerPath: "apidestination",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5001/" }),
+  endpoints: (builder) => ({
+    getAllDestination: builder.query({
+      query: () => "destination",
+    }),
+  }),
+});
+
+export const { useGetAllDestinaionQuery } = destinationAPI;
