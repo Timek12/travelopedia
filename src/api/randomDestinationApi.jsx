1a2a904cfeee76fd9b058698a9f74a4f5f4da0de
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-12 00:46:01 +0100

    Add configuration to have another api (random-api)

---
+++ src/api/randomDestinationApi.jsx
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const randomDestinationAPI = createApi({
+  reducerPath: "apiRandomDestination",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://random-data-api.com/api/v2/" }),
+  tagTypes: ["Destinations"],
+  endpoints: (builder) => ({
+    getRandomDestination: builder.query({
+      query: () => ({
+        url: "addresses",
+        method: "GET",
+      }),
+    }),
+  }),
+});
+
+export const { useGetRandomDestinationQuery } = randomDestinationAPI;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { destinationAPI } from "../api/destinationApi";
+import { randomDestinationAPI } from "../api/randomDestinationApi";
 
 export const store = configureStore({
   reducer: {
     [destinationAPI.reducerPath]: destinationAPI.reducer,
+    [randomDestinationAPI.reducerPath]: randomDestinationAPI.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(destinationAPI.middleware),
+    getDefaultMiddleware()
+      .concat(destinationAPI.middleware)
+      .concat(randomDestinationAPI.middleware),
 });
