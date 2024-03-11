0a139ecaf32b36614cd55f124f2fc6393e1dc8d1
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-11 14:34:37 +0100

    Append AddDestination component with basic form for adding new destination

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./layout/Header";
+import AddDestination from "./components/AddDestination";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Header />
+    <AddDestination/>
   </React.StrictMode>
 );
